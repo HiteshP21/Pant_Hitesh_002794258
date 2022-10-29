837e55250e1c793814af050855b8ab396c2796a6
HiteshP21 <pant.hit@northeastern.edu>
2022-10-29 12:05:10 -0400

    Vital Signs added

---
+++ Hospital_Mgm_sys/src/main/java/model/VitalSigns.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *bp, height heart rate, temps, respiration.
+ * @author tarush
+ */
+public class VitalSigns extends Patient{
+    private int heartRate;
+    private int pulse;
+    private int bp;
+    private double temp;
+    
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+    public int getBp() {
+        return bp;
+    }
+
+    public void setBp(int bp) {
+        this.bp = bp;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int Temp) {
+        this.temp = temp;
+    }
+}
