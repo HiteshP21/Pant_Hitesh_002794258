7d7e7932f04fb00bf1f7ce540a9bb06eb33222ed
HiteshP21 <pant.hit@northeastern.edu>
2022-10-30 04:16:29 -0400

    Hospital class created

---
+++ Hospital_Mgm_sys/src/main/java/model/Hospital.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Hitesh
+ */
+public class Hospital {
+    private String hopName;
+    private int hospId;
+    private String hospAdd;
+    private String hospCom;
+
+    public String getHopName() {
+        return hopName;
+    }
+
+    public void setHopName(String hopName) {
+        this.hopName = hopName;
+    }
+
+    public int getHospId() {
+        return hospId;
+    }
+
+    public void setHospId(int hospId) {
+        this.hospId = hospId;
+    }
+
+    public String getHospAdd() {
+        return hospAdd;
+    }
+
+    public void setHospAdd(String hospAdd) {
+        this.hospAdd = hospAdd;
+    }
+
+    public String getHospCom() {
+        return hospCom;
+    }
+
+    public void setHospCom(String hospCom) {
+        this.hospCom = hospCom;
+    }
+}
